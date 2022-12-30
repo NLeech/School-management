c31eafb812934dbf5a26c44e643577265e40c93d
Alexander Braylovskiy <alexander.braylovskiy@gmail.com>
2022-12-30 15:26:12 +0200

    Docker compose initial support

---
+++ docker_postgres_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+psql -U postgres -c "CREATE USER $PG_USER WITH ENCRYPTED PASSWORD '$PASSWD';"
+psql -U postgres -c "ALTER USER $PG_USER CREATEDB;"
+psql -U postgres -c "DROP DATABASE IF EXISTS $PG_DATABASE;"
+psql -U postgres -c "CREATE DATABASE $PG_DATABASE;"
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE $PG_DATABASE TO $PG_USER;"
+psql  -U postgres -d $PG_DATABASE -c "GRANT ALL ON SCHEMA public TO $PG_USER;"
