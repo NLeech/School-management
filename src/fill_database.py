2a8d87909843d89ee5e0981d6f26b64c7c45d012
Alexander Braylovskiy <alexander.braylovskiy@gmail.com>
2022-12-31 22:21:14 +0200

    Docker compose: fill the database with random data with "fill" profile

--- src/fill_database.py
+++ src/fill_database.py
@@ -4,3 +4,4 @@ from school_management import create_database_connection
 if __name__ == "__main__":
     db = create_database_connection()
     db.fill_database()
+    print("Database was filled with random data!")
